86eaa877e23e5ebada4e1ea62922cf1be71320ef
takeseem <takeseem@gmail.com>
2024-10-31 11:34:47 +0800

    项目主页加闪光，博主信息移到左侧，图标悬停跳动，hr 加驾驶的汽车

---
+++ src/.vuepress/client.ts
@@ -0,0 +1,4 @@
+import "vuepress-theme-hope/presets/shinning-feature-panel.scss";
+import "vuepress-theme-hope/presets/left-blog-info.scss";
+import "vuepress-theme-hope/presets/bounce-icon.scss";
+import "vuepress-theme-hope/presets/hr-driving-car.scss";
